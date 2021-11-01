1e75c13a1dc12ba67e9f2bd92039c08cd4a4bcd4
Ashwin Rohit <aalagiri@ucsd.edu>
2021-10-31 17:21:44 -0700

    Add cli options parsing and rand generator
    Add methods for parsing command line arguments (currently only the wordlist) and
    read the file to generate a random subset of words of given length.

--- include/word_generator.hpp
+++ include/word_generator.hpp
@@ -1,47 +1,74 @@
 #pragma once
+#include <random>
+#include <ctime>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <optional>
 #include "argparse.hpp"
 
 using namespace std;
+using uint = unsigned int;
 
-class WordGenerator {
+class TypeTest{
 private:
 
-	char* m_filename;
-	void test_vector(vector<string> vec) {
-		cout << "[";
-		for(auto &i: vec) {
-			cout << i << " ";
-		} cout << "]\n";
+	
+	string wordlist_path;
+	
+	void print_vector(const vector<string>& str_vec) const {
+		for(auto &i: str_vec) {
+			cout << i << endl;
+		}
+	}
+	
+	string parse_file_path_from_arg(int argc, char** argv) {
+		argparse::ArgumentParser options("tty-typing");
+		options.add_argument("-w", "--wordlist")
+			.help("Path to wordlist")
+			.required();
+		try {
+			options.parse_args(argc, argv);
+		} 
+		catch (const std::runtime_error& err) {
+			cerr << "Error: " << err.what() << endl;
+			exit(1);
+		}
+		string word_list_path = options.get<string>("--wordlist");
+		return word_list_path;
 	}
 
-	char* parse_file_name_from_arg(int argc, char** argv) {
-		argparse::ArgumentParser program("tty-type-test");
-		program.add_argument("-w", "wordlist")
-			.help("Path to file containing the words")
-			.required();
-		return nullptr;
+	vector<string> read_words_from_list() {
+		string word;
+		ifstream wordlist_file;
+		wordlist_file.open(this -> wordlist_path);
+		vector<string> words;
+
+		while(wordlist_file >> word) {
+			words.push_back(word);
+		}
+		cout << "ALL WORDS" << endl;
+		print_vector(words);
+		return words;
+	}
+
+	vector<string> generate_random_words(vector<string> all_words, uint num_words) {
+		random_device rnd;
+		mt19937 rng(rnd());
+		uniform_int_distribution<uint> rnums(0, all_words.size()-1);
+		vector<string> random_words;
+		for(uint i = 0; i < num_words; ++i) {
+			random_words.push_back(all_words[rnums(rng)]);
+		}
+		cout << "RANDOM WORDS" << endl;
+		print_vector(random_words);
+		return random_words;
 	}
 
 public:
-	vector<string> read_words(char* filename) {
-		ifstream infile;
-		infile.open(this -> m_filename, ios::in);
-
-		string word;
-		vector<string> words;
-
-		while(!infile.eof()) {
-			getline(infile, word);
-			if(word.size() > 0) {
-				words.push_back(word);
-			}
-		}
-		test_vector(words);
-		return words;
+	void initialize(int argc, char** argv) {
+		this -> wordlist_path = parse_file_path_from_arg(argc, argv);
+		vector<string> all_words = read_words_from_list();
+		generate_random_words(all_words, 25);
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "../include/word_generator.hpp"
 
 int main(int argc, char** argv) {
-    WordGenerator wg;
-    // Temp filename for testing
-    wg.read_words(argv[1]);
+	TypeTest tt; 
+	tt.initialize(argc, argv);
 }
